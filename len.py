8731c5f516f7a4515143ff7fe3762a57263861a1
Your Name <you@example.com>
2017-08-09 11:13:07 +0530

    New commit

---
+++ len.py
@@ -0,0 +1,4 @@
+a = len("") > 0
+b = len("Hello") > 0
+c = len(" ") > 0
+print (a,b,c)
---
+++ num.py
@@ -0,0 +1,5 @@
+num = int(input("enter num "))
+if num % 2==0:
+    print ("%d is even" % (num))
+else:
+        print("%d num is odd"%(num))
